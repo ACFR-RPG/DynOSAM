23f7bc1e7c57a8c678d3fbc1dc00ddb1e6481f5d
YiduoWang <yiduowang47@gmail.com>
2026-07-01 11:23:27 +1000

    Update docker mount volumns

--- docker/create_container_amd64.sh
+++ docker/create_container_amd64.sh
@@ -2,9 +2,9 @@
 
 ### EDIT THIS TO WHEREVER YOU'RE STORING YOU DATA ###
 # folder should exist before you mount it
-LOCAL_DATA_FOLDER=/media/jmorris/T7/datasets/
-LOCAL_RESULTS_FOLDER=~/Code/src/dynosam/results/
-LOCAL_DYNO_SAM_FOLDER=~/Code/src/dynosam/DynOSAM
-LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER=~/Code/src/dynosam/extras/
+LOCAL_DATA_FOLDER=~/logs_dynosam/
+LOCAL_RESULTS_FOLDER=~/results_dynosam/
+LOCAL_DYNO_SAM_FOLDER=~/git_dynosam/DynOSAM
+LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER=~/git_dynosam/extras/
 
 bash create_container_base.sh acfr_rpg/dyno_sam_cuda dyno_sam $LOCAL_DATA_FOLDER $LOCAL_RESULTS_FOLDER $LOCAL_DYNO_SAM_FOLDER $LOCAL_THIRD_PARTY_DYNO_SAM_FOLDER
